11e67a7f52cade1f928ea3dcd178bb5d2e302d0a
Kapil19-dev <rsingh2@student.nitw.ac.in>
2021-07-22 09:53:26 +0530

    addition of model done

--- DL_BASED_METHOD/testmodel.py
+++ DL_BASED_METHOD/testmodel.py
@@ -1,10 +1,10 @@
-from tf_model import BRUnet
+from tf_model import BRUnet,BRUnet_Multi_resp,BRUnet_Encoder
 import tensorflow as tf
 
 #import pdb;pdb.set_trace()
 input_shape = (128, 128, 3)
 model_input_shape = (128, 3)
 rand_input = tf.random.uniform(input_shape)
-model = BRUnet(model_input_shape)
-output = model(rand_input)
-print(output)
+model = BRUnet_Multi_resp(model_input_shape)
+output,out_4 = model(rand_input)
+print(out_4.shape)
