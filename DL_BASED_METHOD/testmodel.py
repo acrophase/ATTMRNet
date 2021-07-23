52a35c3c9adb09f77e399423ca198cd8ded31aca
Kapil19-dev <ksr19nitw@gmail.com>
2021-07-23 19:24:47 +0530

    new model added

--- DL_BASED_METHOD/testmodel.py
+++ DL_BASED_METHOD/testmodel.py
@@ -1,10 +1,10 @@
-from tf_model import BRUnet,BRUnet_Multi_resp,BRUnet_Encoder
+from tf_model import BRUnet,BRUnet_Multi_resp,BRUnet_Encoder,BRUnet_raw
 import tensorflow as tf
 
 #import pdb;pdb.set_trace()
-input_shape = (128, 128, 3)
-model_input_shape = (128, 3)
+input_shape = (128, 2048, 3)
+model_input_shape = (2048, 3)
 rand_input = tf.random.uniform(input_shape)
-model = BRUnet_Multi_resp(model_input_shape)
-output,out_4 = model(rand_input)
-print(out_4.shape)
+model = BRUnet_raw(model_input_shape)
+output = model(rand_input)
+print(output)
