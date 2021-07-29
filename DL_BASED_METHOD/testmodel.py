f29f402d59450a00b9aaf7fb55c4cfbcd35d224b
Kapil19-dev <ksr19nitw@gmail.com>
2021-07-29 12:41:10 +0530

    changed the code of new conf

--- DL_BASED_METHOD/testmodel.py
+++ DL_BASED_METHOD/testmodel.py
@@ -1,10 +1,11 @@
-from tf_model import BRUnet,BRUnet_Multi_resp,BRUnet_Encoder,BRUnet_raw
+from tf_model import BRUnet,BRUnet_Multi_resp,BRUnet_Encoder,BRUnet_raw,BRUnet_raw_encoder,BRUnet_raw_multi
 import tensorflow as tf
 
 #import pdb;pdb.set_trace()
 input_shape = (128, 2048, 3)
 model_input_shape = (2048, 3)
 rand_input = tf.random.uniform(input_shape)
-model = BRUnet_raw(model_input_shape)
-output = model(rand_input)
-print(output)
+model = BRUnet_raw_multi(model_input_shape)
+output,out_4  = model(rand_input)
+print(output.shape)
+print(out_4.shape)
