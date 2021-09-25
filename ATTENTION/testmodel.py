e568b81630cce84a3d37b70b780d814d0631d6cf
Kapil19-dev <rsingh2@student.nitw.ac.in>
2021-09-25 19:24:46 +0530

    attention block checked

--- ATTENTION/testmodel.py
+++ ATTENTION/testmodel.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
-from tf_model import BRUnet
+from tf_model import BRUnet_raw_encoder
 
 #import pdb;pdb.set_trace()
-input_shape = (128, 128, 3)
-model_input_shape1 = (128, 3)
+input_shape = (128, 2048, 3)
+model_input_shape1 = (2048, 3)
 #model_input_shape2 = (128, 3)
 rand_input1 = tf.random.uniform(input_shape)
 #rand_input2 = tf.random.uniform(input_shape)
-model = BRUnet(model_input_shape1)
-output  = model(rand_input1)
+model= BRUnet_raw_encoder(model_input_shape1)
+output,_,_,_,_  = model(rand_input1)
 print(output.shape)
