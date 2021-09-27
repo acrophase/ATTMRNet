8847dd6b67106597da5f2f8adf1d2b0f457c9a3f
Kapil19-dev <rsingh2@student.nitw.ac.in>
2021-09-27 19:39:07 +0530

    changes added

--- ATTENTION/testmodel.py
+++ ATTENTION/testmodel.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tf_model import BRUnet_raw_encoder
+from tf_model import BRUnet_raw_multi
 
 #import pdb;pdb.set_trace()
 input_shape = (128, 2048, 3)
@@ -7,6 +7,8 @@ model_input_shape1 = (2048, 3)
 #model_input_shape2 = (128, 3)
 rand_input1 = tf.random.uniform(input_shape)
 #rand_input2 = tf.random.uniform(input_shape)
-model= BRUnet_raw_encoder(model_input_shape1)
-output,_,_,_,_  = model(rand_input1)
+model= BRUnet_raw_multi(model_input_shape1)
+output,out_4  = model(rand_input1)
 print(output.shape)
+print(out_4.shape)
+
