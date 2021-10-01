41da7fccde9acc8b44b5e0dda44cda0650ec87b0
Kapil19-dev <rsingh2@student.nitw.ac.in>
2021-10-01 19:08:07 +0530

    attention model -5

--- ATTENTION/testmodel.py
+++ ATTENTION/testmodel.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tf_model import BRUnet_raw_multi
+from tf_model_new import BRUnet_raw
 
 #import pdb;pdb.set_trace()
 input_shape = (128, 2048, 3)
@@ -7,8 +7,8 @@ model_input_shape1 = (2048, 3)
 #model_input_shape2 = (128, 3)
 rand_input1 = tf.random.uniform(input_shape)
 #rand_input2 = tf.random.uniform(input_shape)
-model= BRUnet_raw_multi(model_input_shape1)
-output,out_4,attn1,attn2,attn3,attn4,attn5,attn6,attn7,attn8,attn9,attn10,attn11  = model(rand_input1)
+model= BRUnet_raw(model_input_shape1)
+output  = model(rand_input1)
 print(output.shape)
-print(out_4.shape)
+
 
