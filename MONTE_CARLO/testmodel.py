fc5042aa1af34451ad63c929b590e7594e4bdc28
Kapil19-dev <rsingh2@student.nitw.ac.in>
2021-10-20 11:47:34 +0530

    chanegs added

--- MONTE_CARLO/testmodel.py
+++ MONTE_CARLO/testmodel.py
@@ -2,9 +2,9 @@ from tf_model import BRUnet,BRUnet_Multi_resp,BRUnet_Encoder,BRUnet_raw,BRUnet_r
 import tensorflow as tf
 
 #import pdb;pdb.set_trace()
-input_shape = (128, 128, 3)
-model_input_shape = (128, 3)
+input_shape = (128, 2048, 3)
+model_input_shape = (2048, 3)
 rand_input = tf.random.uniform(input_shape)
-model = BRUnet(model_input_shape)
-output  = model(rand_input)
+model = BRUnet_raw(model_input_shape)
+output,_,_,_,_  = model(rand_input)
 print(output.shape)
