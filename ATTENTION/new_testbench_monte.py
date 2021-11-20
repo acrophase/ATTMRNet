66350dd0852e7efe483796f311a616b7c1e49b5e
Kapil19-dev <rsingh2@student.nitw.ac.in>
2021-11-20 17:21:38 +0530

    corrections made

--- ATTENTION/new_testbench_monte.py
+++ ATTENTION/new_testbench_monte.py
@@ -487,7 +487,7 @@ for item in config_list:
             if epoch <=20:
                 lr = 1e-4
             else:
-                lr = 1e-5
+                lr = 1e-4
             return lr
         model_input_shape = (2048,3)
         model  = BRUnet_raw_multi_ATT_MC(model_input_shape)
@@ -551,7 +551,7 @@ for item in config_list:
             if mean_loss < best_loss:
                 best_loss = mean_loss
                 #model.save_weights(os.path.join(results_path, 'best_model_5'+str(1e-5)+'_'+str(num_epochs)+'.h5'))
-                model.save_weights(os.path.join(results_path, 'best_model_2'+str(1e-4)+'_'+str(1e-5)+'_'+str(num_epochs)+'.h5'))
+                model.save_weights(os.path.join(results_path, 'best_model_1'+str(1e-4)+'_'+str(1e-4)+'_'+str(num_epochs)+'.h5'))
             print("validation loss -- {}".format(mean_loss))
             #print(test_loss.result())
             train_loss.reset_states()
