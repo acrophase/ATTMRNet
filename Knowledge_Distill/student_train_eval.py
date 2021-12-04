c1200e71ca3f919311e43ad4b28cd637e7f8b9b6
Kapil19-dev <rsingh2@student.nitw.ac.in>
2021-12-04 19:48:10 +0530

    changes added

--- Knowledge_Distill/student_train_eval.py
+++ Knowledge_Distill/student_train_eval.py
@@ -196,7 +196,7 @@ for epoch in tqdm.tqdm(range(num_epochs)):
     if mean_loss < best_loss:
         best_loss = mean_loss
             #model.save_weights(os.path.join(results_path, 'best_model_1'+str(1e-3)+'_'+str(num_epochs)+'.h5'))
-        model.save_weights(os.path.join(results_path, 'best_model_4'+str(1e-2)+'_'+str(1e-5)+'_'+str(num_epochs)+'.h5'))
+        model.save_weights(os.path.join(results_path, 'best_model_5'+str(1e-2)+'_'+str(1e-5)+'_'+str(num_epochs)+'.h5'))
     print("validation loss -- {}".format(mean_loss))
     print(test_loss.result())
     train_loss.reset_states()
