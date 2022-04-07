2e39f2e2390682d330f901bf6049affc0f215d34
Kapil19-dev <rsingh2@student.nitw.ac.inm>
2022-04-07 12:00:44 +0530

    new models added

--- Knowledge_Distill/testmodel.py
+++ Knowledge_Distill/testmodel.py
@@ -1,4 +1,4 @@
-from student_model import BRUnet_Multi_resp_student_2
+from student_model import BRUnet_Multi_resp_student_5
 from teacher_model import BRUnet_Multi_resp_ATT_MC
 import tensorflow as tf
 
@@ -6,7 +6,7 @@ import tensorflow as tf
 input_shape = (128, 128, 3)
 model_input_shape = (128, 3)
 rand_input = tf.random.uniform(input_shape)
-model = BRUnet_Multi_resp_ATT_MC(model_input_shape)
-output,out_4  = model(rand_input)
+model = BRUnet_Multi_resp_student_5(model_input_shape)
+output,out_4,e6,attn1,attn2,attn4, attn9  = model(rand_input)
 print(output.shape)
 print(out_4.shape)
